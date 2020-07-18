14907586a698e65f8e5601d38823cc1516cf9551
chromanoiz <chayanne624@gmail.com>
2020-07-17 17:54:03 -0700

    added emote

--- commands/rick.js
+++ commands/rick.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'rick',
     description: 'Let rick know his actions are disappointing',
     execute (message, args) {
-        message.channel.send('fuck you rick');
+        message.channel.send('fuck you rick <:Angry:701947676308013067>');
     },
 };
