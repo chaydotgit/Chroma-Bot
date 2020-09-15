cc63137b0cc21aec69db21539f35f29d4bd89d46
Chayanne <chayanne624@gmail.com>
2020-09-15 14:13:42 -0700

    play! command is fixed, updated some node-modules

--- commands/reload.js
+++ commands/reload.js
@@ -8,14 +8,14 @@ module.exports = {
             || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
         if (!command) 
-            return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+            return message.channel.send(`There is no command with name or alias [${commandName}], ${message.author}!`);
         
         delete require.cache[require.resolve(`./${command.name}.js`)];
 
         try {
             const newCommand = require(`./${command.name}.js`);
             message.client.commands.set(newCommand.name, newCommand);
-            message.channel.send(`Command \'$command.name}\` was reloaded!`);
+            message.channel.send(`Command [${command.name}] was reloaded!`);
         } catch (error) {
             console.log(error);
             message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
