858cbf5ca4fb66ee0100756605fc2524ccc29b5e
Chayanne Rodriguez <chayanne624@gmail.com>
2022-01-14 16:51:42 -0800

    Reimplemented /help command #8

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Lists all commands that CHROMA bot can perform.'),
+    async execute (interaction) {
+        const embed = new MessageEmbed()
+            .setColor('#fc8c03')
+            .setTitle('I heard you need help? ⚙️')
+            .setThumbnail('https://i.imgur.com/HHmfcpT.jpg')
+            .setDescription('Here are all the commands I can perform for you...')
+            .addFields(
+                {name: '/hello', value: 'I will say hi back to you. :^)'},
+                {name: '/color', value: 'Change your color on the server with a select menu I send.'},
+                {name: '/pronouns', value: 'Set your preferred pronouns as a role on the server with the select menu I send.'},
+                {name: '/anime', value: 'Want info about an anime? Send me the title and I will give you basic information about it.'},
+                {name: '/glados', value: 'I say a GLaDOS voice line'},
+            )
+            .setFooter({ text: 'C H R O M A', iconURL: 'https://i.imgur.com/HHmfcpT.jpg' });
+            await interaction.reply({embeds: [embed]});
+    },
+};
