0fe94d738e4f45bc6d5b439ce5a4542d8f648373
Chayanne Rodriguez <chayanne624@gmail.com>
2023-02-28 15:55:41 -0800

    refactored pronoun and color selector code

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#fc8c03')
             .setTitle('I heard you need help? ⚙️')
-            .setThumbnail('https://i.imgur.com/HHmfcpT.jpg')
+            .setThumbnail('https://i.imgur.com/KXtuGW5.png')
             .setDescription('Here are all the commands I can perform for you...')
             .addFields(
                 {name: '/hello', value: 'I will say hi back to you. :^)'},
@@ -17,7 +17,7 @@ module.exports = {
                 {name: '/anime', value: 'Want info about an anime? Send me the title and I will give you basic information about it.'},
                 {name: '/glados', value: 'I say a GLaDOS voice line'},
             )
-            .setFooter({ text: 'C H R O M A', iconURL: 'https://imgur.com/KXtuGW5' });
+            .setFooter({ text: 'C H R O M A', iconURL: 'https://i.imgur.com/KXtuGW5.png' });
             await interaction.reply({embeds: [embed]});
     },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -9,47 +9,59 @@ module.exports = {
 
         if (interaction.isStringSelectMenu()) {
             if (interaction.customId == 'color select') {
-                const existingColors = ["PURPLE", "PINK", "RED PINK", "RED", "ORANGE", "YELLOW", "GREEN", "MINT GREEN", "TEAL", "BLUE", "WHITE", "GREY", "BLACK"]
-
-                let newColor = interaction.guild.roles.cache.find(r => r.name === interaction.values[0]);
-                let currentColor = interaction.member.roles.cache.find(r => existingColors.includes(r.name))
-                if (currentColor) {
-                    interaction.member.roles.remove(currentColor);
-                }
-                interaction.member.roles.add(newColor);
-                await interaction.reply({ content: `${interaction.member.displayName}'s color has been changed to ${newColor.name}!`, ephemeral: true });
-                console.log(`${interaction.user.tag} changed their role color to ${newColor.name}`);
+                await colorSelect();
             }
-
             if (interaction.customId == 'pronoun select') {
-                let newPronouns = [];
-                let existingPronouns = []
-                for (const pronoun of interaction.values) {
-                    let hasPronoun = interaction.member.roles.cache.find(r => r.name === pronoun);
-                    if (!hasPronoun) {
-                        newPronouns.push(pronoun);
-                        interaction.member.roles.add(interaction.guild.roles.cache.find(r => r.name === pronoun));
-                    } else {
-                        existingPronouns.push(pronoun);
-                    }
-                }
-
-                if (newPronouns.length != 0) {
-                    await interaction.reply({ content: `${interaction.member.displayName}'s pronouns are: ${newPronouns.concat(existingPronouns).toString()}` });
-                } else {
-                    await interaction.reply({ content: `You have already added the pronouns ${existingPronouns.toString()}`, ephemeral: true});
-                }
+                await pronounSelect();
             }
+            return;
         }
 
         const command = interaction.client.commands.get(interaction.commandName);
-        if (!command) return;
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
 
         try {
             await command.execute(interaction);
         } catch (error) {
-            console.error(error);
+            console.error(`Error executing ${interaction.commandName}`);
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+        }
+        
+
+        async function colorSelect() {
+            const existingColors = ["PURPLE", "PINK", "RED PINK", "RED", "ORANGE", "YELLOW", "GREEN", "MINT GREEN", "TEAL", "BLUE", "WHITE", "GREY", "BLACK"];
+
+            let newColor = interaction.guild.roles.cache.find(r => r.name === interaction.values[0]);
+            let currentColor = interaction.member.roles.cache.find(r => existingColors.includes(r.name));
+            if (currentColor) {
+                interaction.member.roles.remove(currentColor);
+            }
+            interaction.member.roles.add(newColor);
+            await interaction.reply({ content: `${interaction.member.displayName}'s color has been changed to ${newColor.name}!`, ephemeral: true });
+            console.log(`${interaction.user.tag} changed their role color to ${newColor.name}`);
+        }
+        
+        async function pronounSelect() {
+            let newPronouns = [];
+            let existingPronouns = [];
+            for (const pronoun of interaction.values) {
+                let hasPronoun = interaction.member.roles.cache.find(r => r.name === pronoun);
+                if (!hasPronoun) {
+                    newPronouns.push(pronoun);
+                    interaction.member.roles.add(interaction.guild.roles.cache.find(r => r.name === pronoun));
+                } else {
+                    existingPronouns.push(pronoun);
+                }
+            }
+
+            if (newPronouns.length != 0) {
+                await interaction.reply({ content: `${interaction.member.displayName}'s pronouns are: ${newPronouns.concat(existingPronouns).toString()}` });
+            } else {
+                await interaction.reply({ content: `You have already added the pronouns ${existingPronouns.toString()}`, ephemeral: true });
+            }
         }
     },
 }
